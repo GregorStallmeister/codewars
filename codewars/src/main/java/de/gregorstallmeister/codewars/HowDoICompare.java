e0183207c901b090447ec6ee06f38d5e2bb40d09
GregorStallmeister <post@gregor-stallmeister.de>
2025-05-14 11:52:00 +0200

    solve HowDoICompare

---
+++ codewars/src/main/java/de/gregorstallmeister/codewars/HowDoICompare.java
@@ -0,0 +1,27 @@
+package de.gregorstallmeister.codewars;
+
+import java.util.Map;
+
+public class HowDoICompare {
+    public static String whatIsFirst(Integer x) {
+        for (Object[] p : specialNumbers) {
+            if (p[0].equals(x))
+                return (String)p[1];
+        }
+        return "nothing";
+    }
+
+    static final Object[][] specialNumbers = {
+            {42, "everything"},
+            {1764, "everything squared"},
+    };
+
+    static final Map<Integer, String> SPECIAL_NUMBERS = Map.ofEntries(
+            Map.entry(42, "everything"),
+            Map.entry(42 * 42, "everything squared")
+    );
+
+    public static String whatIs(Integer x) {
+        return SPECIAL_NUMBERS.getOrDefault(x, "nothing");
+    }
+}
---
+++ codewars/src/test/java/de/gregorstallmeister/codewars/test/HowDoICompareTest.java
@@ -0,0 +1,23 @@
+package de.gregorstallmeister.codewars.test;
+
+import de.gregorstallmeister.codewars.HowDoICompare;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+// https://www.codewars.com/kata/55d8618adfda93c89600012e/train/java
+
+public class HowDoICompareTest {
+    @Test
+    public void test() throws Exception {
+        for (Object[] t: tests)
+            assertEquals(t[1], HowDoICompare.whatIs((Integer)t[0]));
+    }
+
+    static final Object[][] tests = {
+            {0, "nothing"},
+            {123, "nothing"},
+            {-1, "nothing"},
+            {42, "everything"},
+            {42 * 42, "everything squared"},
+    };
+}
