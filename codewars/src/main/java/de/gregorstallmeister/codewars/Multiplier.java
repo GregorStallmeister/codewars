506a8359236e7eae882c2dbdb3236276d292be4b
GregorStallmeister <post@gregor-stallmeister.de>
2025-05-22 12:16:06 +0200

    solve Overflowing with joy

---
+++ codewars/src/main/java/de/gregorstallmeister/codewars/Multiplier.java
@@ -0,0 +1,7 @@
+package de.gregorstallmeister.codewars;
+
+public class Multiplier {
+    public static int multiply(int a, int b) throws ArithmeticException {
+        return Math.multiplyExact(a, b);
+    }
+}
