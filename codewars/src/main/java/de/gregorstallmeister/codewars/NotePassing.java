6d5890a072a3e7e5b01c41b571dee745d60aaf48
GregorStallmeister <post@gregor-stallmeister.de>
2025-05-26 17:14:45 +0200

    solve NotePassing

---
+++ codewars/src/main/java/de/gregorstallmeister/codewars/NotePassing.java
@@ -0,0 +1,68 @@
+package de.gregorstallmeister.codewars;
+
+public class NotePassing {
+    public static String decipher(String codedMessage){
+        char[] codedMessageChars = codedMessage.toCharArray();
+        StringBuilder decipheredMessageBuilder = new StringBuilder();
+
+        for (char c: codedMessageChars) {
+            switch (c) {
+                case 'A' -> decipheredMessageBuilder.append('H');
+                case 'a' -> decipheredMessageBuilder.append('h');
+                case 'B' -> decipheredMessageBuilder.append('I');
+                case 'b' -> decipheredMessageBuilder.append('i');
+                case 'C' -> decipheredMessageBuilder.append('J');
+                case 'c' -> decipheredMessageBuilder.append('j');
+                case 'D' -> decipheredMessageBuilder.append('K');
+                case 'd' -> decipheredMessageBuilder.append('k');
+                case 'E' -> decipheredMessageBuilder.append('L');
+                case 'e' -> decipheredMessageBuilder.append('l');
+                case 'F' -> decipheredMessageBuilder.append('M');
+                case 'f' -> decipheredMessageBuilder.append('m');
+                case 'G' -> decipheredMessageBuilder.append('N');
+                case 'g' -> decipheredMessageBuilder.append('n');
+                case 'H' -> decipheredMessageBuilder.append('O');
+                case 'h' -> decipheredMessageBuilder.append('o');
+                case 'I' -> decipheredMessageBuilder.append('P');
+                case 'i' -> decipheredMessageBuilder.append('p');
+                case 'J' -> decipheredMessageBuilder.append('7');
+                case 'j' -> decipheredMessageBuilder.append('7');
+                case 'K' -> decipheredMessageBuilder.append('R');
+                case 'k' -> decipheredMessageBuilder.append('r');
+                case 'L' -> decipheredMessageBuilder.append('S');
+                case 'l' -> decipheredMessageBuilder.append('s');
+                case 'M' -> decipheredMessageBuilder.append('T');
+                case 'm' -> decipheredMessageBuilder.append('t');
+                case 'N' -> decipheredMessageBuilder.append('U');
+                case 'n' -> decipheredMessageBuilder.append('u');
+                case 'O' -> decipheredMessageBuilder.append('V');
+                case 'o' -> decipheredMessageBuilder.append('v');
+                case 'P' -> decipheredMessageBuilder.append('W');
+                case 'p' -> decipheredMessageBuilder.append('w');
+                case 'Q' -> decipheredMessageBuilder.append('7');
+                case 'q' -> decipheredMessageBuilder.append('7');
+                case 'R' -> decipheredMessageBuilder.append('Y');
+                case 'r' -> decipheredMessageBuilder.append('y');
+                case 'S' -> decipheredMessageBuilder.append('7');
+                case 's' -> decipheredMessageBuilder.append('7');
+                case 'T' -> decipheredMessageBuilder.append('A');
+                case 't' -> decipheredMessageBuilder.append('a');
+                case 'U' -> decipheredMessageBuilder.append('B');
+                case 'u' -> decipheredMessageBuilder.append('b');
+                case 'V' -> decipheredMessageBuilder.append('C');
+                case 'v' -> decipheredMessageBuilder.append('c');
+                case 'W' -> decipheredMessageBuilder.append('D');
+                case 'w' -> decipheredMessageBuilder.append('d');
+                case 'X' -> decipheredMessageBuilder.append('E');
+                case 'x' -> decipheredMessageBuilder.append('e');
+                case 'Y' -> decipheredMessageBuilder.append('F');
+                case 'y' -> decipheredMessageBuilder.append('f');
+                case 'Z' -> decipheredMessageBuilder.append('G');
+                case 'z' -> decipheredMessageBuilder.append('g');
+                default -> decipheredMessageBuilder.append(c);
+            }
+        }
+
+        return decipheredMessageBuilder.toString();
+    }
+}
