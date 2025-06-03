a12593df8a20cf006e9de77351dcd39d6b550c34
GregorStallmeister <post@gregor-stallmeister.de>
2025-06-03 15:07:57 +0200

    solve CreatePhoneNumber

---
+++ codewars/src/main/java/de/gregorstallmeister/codewars/CreatePhoneNumber.java
@@ -0,0 +1,16 @@
+package de.gregorstallmeister.codewars;
+
+import java.util.stream.IntStream;
+
+public class CreatePhoneNumber {
+    public static String createPhoneNumberFirst(int[] numbers) {
+        return String.format("(%d%d%d) %d%d%d-%d%d%d%d",
+                numbers[0], numbers[1], numbers[2],
+                numbers[3], numbers[4], numbers[5],
+                numbers[6], numbers[7], numbers[8], numbers[9]);
+    }
+
+    public static String createPhoneNumber(int[] numbers) {
+        return String.format("(%d%d%d) %d%d%d-%d%d%d%d", IntStream.of(numbers).boxed().toArray());
+    }
+}
---
+++ codewars/src/test/java/de/gregorstallmeister/codewars/test/CreatePhoneNumberTest.java
@@ -0,0 +1,19 @@
+package de.gregorstallmeister.codewars.test;
+
+import de.gregorstallmeister.codewars.CreatePhoneNumber;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+// https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/java
+
+public class CreatePhoneNumberTest {
+    @Test
+    public void testsFirst() {
+        assertEquals("(123) 456-7890", CreatePhoneNumber.createPhoneNumberFirst(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+    }
+
+    @Test
+    public void tests() {
+        assertEquals("(123) 456-7890", CreatePhoneNumber.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+    }
+}
