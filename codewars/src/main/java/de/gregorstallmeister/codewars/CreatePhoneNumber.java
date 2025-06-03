d96f2352812d83092c49ecb37e015a7ff86f18f3
GregorStallmeister <post@gregor-stallmeister.de>
2025-06-03 15:11:57 +0200

    solve CreatePhoneNumber in a 3rd way

--- codewars/src/main/java/de/gregorstallmeister/codewars/CreatePhoneNumber.java
+++ codewars/src/main/java/de/gregorstallmeister/codewars/CreatePhoneNumber.java
@@ -10,7 +10,17 @@ public class CreatePhoneNumber {
                 numbers[6], numbers[7], numbers[8], numbers[9]);
     }
 
-    public static String createPhoneNumber(int[] numbers) {
+    public static String createPhoneNumberSecond(int[] numbers) {
         return String.format("(%d%d%d) %d%d%d-%d%d%d%d", IntStream.of(numbers).boxed().toArray());
+    }
+
+    public static String createPhoneNumber(int[] numbers) {
+        String phoneNum = "(xxx) xxx-xxxx";
+
+        for (int n : numbers) {
+            phoneNum = phoneNum.replaceFirst("x", Integer.toString(n));
+        }
+
+        return phoneNum;
     }
 }
--- codewars/src/test/java/de/gregorstallmeister/codewars/test/CreatePhoneNumberTest.java
+++ codewars/src/test/java/de/gregorstallmeister/codewars/test/CreatePhoneNumberTest.java
@@ -13,6 +13,11 @@ public class CreatePhoneNumberTest {
     }
 
     @Test
+    public void testsSecond() {
+        assertEquals("(123) 456-7890", CreatePhoneNumber.createPhoneNumberSecond(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+    }
+
+    @Test
     public void tests() {
         assertEquals("(123) 456-7890", CreatePhoneNumber.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
     }
