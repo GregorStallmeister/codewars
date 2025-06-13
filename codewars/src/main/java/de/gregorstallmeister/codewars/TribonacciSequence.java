d64470e4fc9c3b4806428db02d1faef6e19fecd6
GregorStallmeister <post@gregor-stallmeister.de>
2025-06-13 12:40:42 +0200

    work on TribonacciSequence

---
+++ codewars/src/main/java/de/gregorstallmeister/codewars/TribonacciSequence.java
@@ -0,0 +1,16 @@
+package de.gregorstallmeister.codewars;
+
+public class TribonacciSequence {
+    public double[] tribonacci(double[] s, int n) {
+        double[] result = new double[n];
+        for (int i = 0; i < s.length; i++) {
+            result[i] = s[i];
+        }
+
+        for (int i = 3; i < n; i++) {
+            result[i] = result[i-1] + result[i-2] + result[i-3];
+        }
+
+        return result;
+    }
+}
---
+++ codewars/src/test/java/de/gregorstallmeister/codewars/test/TribonacciSequenceTest.java
@@ -0,0 +1,32 @@
+package de.gregorstallmeister.codewars.test;
+
+import de.gregorstallmeister.codewars.TribonacciSequence;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
+
+// https://www.codewars.com/kata/556deca17c58da83c00002db/train/java
+
+public class TribonacciSequenceTest {
+    private TribonacciSequence variabonacci;
+
+    @Before
+    public void setUp() throws Exception {
+        variabonacci = new TribonacciSequence();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        variabonacci = null;
+    }
+
+    private double precision = 1e-10;
+
+    @Test
+    public void sampleTests() {
+        assertArrayEquals(new double []{1,1,1,3,5,9,17,31,57,105}, variabonacci.tribonacci(new double []{1,1,1},10), precision);
+        assertArrayEquals(new double []{0,0,1,1,2,4,7,13,24,44}, variabonacci.tribonacci(new double []{0,0,1},10), precision);
+        assertArrayEquals(new double []{0,1,1,2,4,7,13,24,44,81}, variabonacci.tribonacci(new double []{0,1,1},10), precision);
+    }
+}
