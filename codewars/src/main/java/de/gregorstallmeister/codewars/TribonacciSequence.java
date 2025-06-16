065eefc5e87268c105a0eb8b61935ed655ddaaa5
GregorStallmeister <post@gregor-stallmeister.de>
2025-06-16 16:29:32 +0200

    solve TribonacciSequence in a shorter way

--- codewars/src/main/java/de/gregorstallmeister/codewars/TribonacciSequence.java
+++ codewars/src/main/java/de/gregorstallmeister/codewars/TribonacciSequence.java
@@ -1,11 +1,23 @@
 package de.gregorstallmeister.codewars;
 
+import java.util.Arrays;
+
 public class TribonacciSequence {
-    public double[] tribonacci(double[] s, int n) {
+    public double[] tribonacciFirst(double[] s, int n) {
         double[] result = new double[n];
         for (int i = 0; i < s.length  && i < n; i++) {
             result[i] = s[i];
         }
+
+        for (int i = 3; i < n; i++) {
+            result[i] = result[i-1] + result[i-2] + result[i-3];
+        }
+
+        return result;
+    }
+
+    public double[] tribonacci(double[] s, int n) {
+        double[] result = Arrays.copyOf(s, n);
 
         for (int i = 3; i < n; i++) {
             result[i] = result[i-1] + result[i-2] + result[i-3];
--- codewars/src/test/java/de/gregorstallmeister/codewars/test/TribonacciSequenceTest.java
+++ codewars/src/test/java/de/gregorstallmeister/codewars/test/TribonacciSequenceTest.java
@@ -24,6 +24,13 @@ public class TribonacciSequenceTest {
     private double precision = 1e-10;
 
     @Test
+    public void sampleTestsFirst() {
+        assertArrayEquals(new double []{1,1,1,3,5,9,17,31,57,105}, variabonacci.tribonacciFirst(new double []{1,1,1},10), precision);
+        assertArrayEquals(new double []{0,0,1,1,2,4,7,13,24,44}, variabonacci.tribonacciFirst(new double []{0,0,1},10), precision);
+        assertArrayEquals(new double []{0,1,1,2,4,7,13,24,44,81}, variabonacci.tribonacciFirst(new double []{0,1,1},10), precision);
+    }
+
+    @Test
     public void sampleTests() {
         assertArrayEquals(new double []{1,1,1,3,5,9,17,31,57,105}, variabonacci.tribonacci(new double []{1,1,1},10), precision);
         assertArrayEquals(new double []{0,0,1,1,2,4,7,13,24,44}, variabonacci.tribonacci(new double []{0,0,1},10), precision);
