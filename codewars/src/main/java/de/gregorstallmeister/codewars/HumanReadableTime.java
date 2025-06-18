b8a1ea74b79aec94c5332f67671b9b56ab9787f1
GregorStallmeister <post@gregor-stallmeister.de>
2025-06-18 15:27:20 +0200

    solve HumanReadableTimeTest in two other ways

--- codewars/src/main/java/de/gregorstallmeister/codewars/HumanReadableTime.java
+++ codewars/src/main/java/de/gregorstallmeister/codewars/HumanReadableTime.java
@@ -1,8 +1,11 @@
 package de.gregorstallmeister.codewars;
 
 
+import java.sql.Time;
+import java.time.Duration;
+
 public class HumanReadableTime {
-    public static String makeReadable(int seconds) {
+    public static String makeReadableFirst(int seconds) {
         StringBuilder stringBuilder = new StringBuilder();
 
         if (seconds < 3600) {
@@ -28,5 +31,15 @@ public class HumanReadableTime {
         stringBuilder.append(seconds < 10 ? "0" + seconds : seconds);
 
         return stringBuilder.toString();
+    }
+
+    public static String makeReadableSecond(int seconds) {
+        return String.format("%02d:%02d:%02d", seconds / 3600, (seconds / 60) % 60, seconds % 60);
+    }
+
+    public static String makeReadable(int seconds) {
+        Duration duration = Duration.ZERO.plusSeconds(seconds);
+
+        return String.format("%02d:%02d:%02d", duration.toHours(), duration.toMinutesPart(), duration.toSecondsPart());
     }
 }
--- codewars/src/test/java/de/gregorstallmeister/codewars/test/HumanReadableTimeTest.java
+++ codewars/src/test/java/de/gregorstallmeister/codewars/test/HumanReadableTimeTest.java
@@ -8,6 +8,24 @@ import static org.junit.Assert.assertEquals;
 
 public class HumanReadableTimeTest {
     @Test
+    public void TestsFirst() {
+        assertEquals("makeReadable(0)", "00:00:00", HumanReadableTime.makeReadableFirst(0));
+        assertEquals("makeReadable(5)", "00:00:05", HumanReadableTime.makeReadableFirst(5));
+        assertEquals("makeReadable(60)", "00:01:00", HumanReadableTime.makeReadableFirst(60));
+        assertEquals("makeReadable(86399)", "23:59:59", HumanReadableTime.makeReadableFirst(86399));
+        assertEquals("makeReadable(359999)", "99:59:59", HumanReadableTime.makeReadableFirst(359999));
+    }
+
+    @Test
+    public void TestsSecond() {
+        assertEquals("makeReadable(0)", "00:00:00", HumanReadableTime.makeReadableSecond(0));
+        assertEquals("makeReadable(5)", "00:00:05", HumanReadableTime.makeReadableSecond(5));
+        assertEquals("makeReadable(60)", "00:01:00", HumanReadableTime.makeReadableSecond(60));
+        assertEquals("makeReadable(86399)", "23:59:59", HumanReadableTime.makeReadableSecond(86399));
+        assertEquals("makeReadable(359999)", "99:59:59", HumanReadableTime.makeReadableSecond(359999));
+    }
+
+    @Test
     public void Tests() {
         assertEquals("makeReadable(0)", "00:00:00", HumanReadableTime.makeReadable(0));
         assertEquals("makeReadable(5)", "00:00:05", HumanReadableTime.makeReadable(5));
