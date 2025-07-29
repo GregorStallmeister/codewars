bd4f12fab5f71e13204ada26a9bb1b3def02886c
GregorStallmeister <post@gregor-stallmeister.de>
2025-07-29 16:47:13 +0200

    start with LoopInspector ( Can you get the loop ?)

---
+++ codewars/src/main/java/de/gregorstallmeister/codewars/LoopInspector.java
@@ -0,0 +1,9 @@
+package de.gregorstallmeister.codewars;
+
+import org.w3c.dom.Node;
+
+public class LoopInspector {
+    public int loopSize(Node node) {
+
+    }
+}
---
+++ codewars/src/test/java/de/gregorstallmeister/codewars/test/LoopInspectorTest.java
@@ -0,0 +1,32 @@
+package de.gregorstallmeister.codewars.test;
+
+import de.gregorstallmeister.codewars.LoopInspector;
+import org.junit.Test;
+import org.w3c.dom.Node;
+
+import static org.junit.Assert.assertEquals;
+
+// https://www.codewars.com/kata/52a89c2ea8ddc5547a000863/train/java
+
+public class LoopInspectorTest {
+    @Test
+    public void loop_size_of_3() {
+        Node list = Node.createChain(1, 3);
+        int result = new LoopInspector().loopSize(list);
+        assertEquals("Incorrect loop size", 3, result);
+    }
+
+    @Test
+    public void loop_size_of_29() {
+        Node list = Node.createChain(21, 29);
+        int result = new LoopInspector().loopSize(list);
+        assertEquals("Incorrect loop size", 29, result);
+    }
+
+    @Test
+    public void loop_size_of_1087() {
+        Node list = Node.createChain(3904, 1087);
+        int result = new LoopInspector().loopSize(list);
+        assertEquals("Incorrect loop size", 1087, result);
+    }
+}
